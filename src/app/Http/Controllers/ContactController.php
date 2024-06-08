29abf84184d27ce14330424551c53159eed8af6e
futo yusa <futo08yuto@outlook.jp>
2024-06-08 23:52:37 +0900

    message

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -13,20 +13,17 @@ class ContactController extends Controller
   } //
 
 
-public function confirm(Request $request)
+public function confirm(ContactRequest $request)
 {
 $contact = $request->only(['name', 'email', 'tel', 'content']);
 return view('confirm', compact('contact'));
 }
 
-public function store(Request $request)
+public function store(ContactRequest $request)
 {
 $contact = $request->only(['name', 'email', 'tel', 'content']);
 Contact::create($contact);
 return view('thanks');
 }
-
-
-
 
 }
